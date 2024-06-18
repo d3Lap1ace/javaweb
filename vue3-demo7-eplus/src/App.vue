77908a28d6d0eec5a0b7cd5206004a0a4aa48288
d3lap1ace L.Lyu <ap1cem@163.com>
2024-06-18 21:13:33 +0800

    update

--- vue3-demo7-eplus/src/App.vue
+++ vue3-demo7-eplus/src/App.vue
@@ -22,7 +22,7 @@ onMounted(()=>{
 // 表格的数据模型
 let hosSetList = ref([])
 // 加载效果
-let loading = ref(true)
+let loading = ref(false)
 
 // 分页
 let pageParams = ref({
@@ -51,6 +51,42 @@ let serchParam = ref({
 let resetSearch=()=>{
   serchParam.value={ }
 }
+// 设置隐藏对话窗 
+let dialogVisible=ref(false)
+// 准备添加或更新医院设置的数据
+let newHospital=ref({
+  id:"",
+  hosname:"",
+  hoscode:"",
+  contactsName:"",
+  contactsPhone:"",
+  apiUrl:""
+})
+// 添加触发显示对话窗
+let showAdd=()=>{
+  dialogVisible.value=true
+  newHospital.value={}
+}
+// 设置对话框显示的标题
+let hospitalTitle=ref("新增医院")
+// 添加所触发的函数
+let addorUpdataHospSet =()=>{
+  dialogVisible.value=false
+  if(!newHospital.value.id){
+    addHospSet()
+  }
+  else{
+    updateHostSet()()
+  }
+}
+// 设置医院添加的函数
+let addHospSet = async()=>{
+  let {code,message} = await add(newHospital.value)
+  if(code == 200){
+    ElMessage.success(message)
+    fetchData()
+  }
+}
 </script>
 
 
@@ -59,29 +95,33 @@ let resetSearch=()=>{
   <el-card style="max-width: 5000px">
     医院名称:<el-input v-model="serchParam.hosName" style="width: 360px" placeholder="请输入要查询的医院名称"/>&nbsp;&nbsp;&nbsp;
     医院编码:<el-input v-model="serchParam.hosCode" style="width: 360px" placeholder="请输入要查询的医院编码"/>&nbsp;
-    <el-button type="primary" plain :icon="Search">搜索</el-button>
+    <el-button type="primary" plain :icon="Search" @click="fetchData">搜索</el-button>
     <el-button plain :iocn="Delete" @click="resetSearch">重置</el-button>
   </el-card>
   <el-card>
     <template #header>
-      
+      <div class ="card-header">
+        <span>
+          <el-button type ="primary" @click="showAdd">新增</el-button>
+        </span>
+      </div>
     </template>
-      <el-table :data="hosSetList" v-loading="loading">
+      <el-table :data="hosSetList" v-loading="loading"  style="width: 100%">
         <el-table-column type="index" label="序号" width="80" />
-        <el-table-column label="医院名称"></el-table-column>
-        <el-table-column label="医院编码"></el-table-column>
-        <el-table-column label="接口地址"></el-table-column>
-        <el-table-column label="联系人"></el-table-column>
-        <el-table-column label="联系电话"></el-table-column>
+        <el-table-column label="医院名称" prop="hosname"></el-table-column>
+        <el-table-column label="医院编码" prop="hoscode"></el-table-column>
+        <el-table-column label="接口地址" prop="apiUrl"></el-table-column>
+        <el-table-column label="联系人" prop="contactsName"></el-table-column>
+        <el-table-column label="联系电话" prop="contactsPhone"></el-table-column>
         <el-table-column label="操作">
           <template #default="scope">
-          <el-button size="small" @click="handleSizeChange(scope.$index, scope.row)">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">
             Edit
           </el-button>
           <el-button
             size="small"
             type="danger"
-            @click="handleCurrentChange(scope.$index, scope.row)"
+            @click="handleDele(scope.$index, scope.row)"
           >
             Delete
           </el-button>
@@ -95,15 +135,44 @@ let resetSearch=()=>{
         :small="small"
         :disabled="disabled"
         :background="background"
-        layout=" total, sizes,prev,next,jumper"
+        layout=" total,prev,next,jumper"
         :total="total"
+        :default-page-size="2"
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
       />
     </template>
   </el-card>
+  <el-dialog :title="hospitalTitle" v-model="dialogVisible" width="70%">
+     <!-- 医院信息收集表单 -->
+      <el-form  label-width="100px" :inline="false" size="normal">
+        <!-- 表单项 -->
+        <el-form-item label="医院名称">
+          <el-input v-model="newHospital.hosname"></el-input>
+        </el-form-item>
+        <el-form-item label="医院编码">
+          <el-input v-model="newHospital.hoscode"></el-input>
+        </el-form-item>
+        <el-form-item label="联系人姓名">
+          <el-input v-model="newHospital.contactsName"></el-input>
+        </el-form-item>
+        <el-form-item label="联系人电话">
+          <el-input v-model="newHospital.contactsPhone"></el-input>
+        </el-form-item>
+        <el-form-item label="接口地址">
+          <el-input v-model="newHospital.apiUrl"></el-input>
+        </el-form-item>
+      </el-form>
+    
+     <!-- 底部取消和确定按钮 -->
+    <template #footer>
+      <span>
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="addorUpdataHospSet">保存</el-button>
+      </span>
+    </template>
+  </el-dialog>
   </div>
- 
 </template>
   
 <style scoped>
